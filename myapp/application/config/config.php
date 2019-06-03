4386bc35ba65d9e84c634275bcc4b27a1d8d943e
Ubuntu <ubuntu@ip-172-31-23-182.us-east-2.compute.internal>
2019-06-03 04:39:52 +0000

    func 100

--- myapp/application/config/config.php
+++ myapp/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://54.190.208.244:8000/index.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
